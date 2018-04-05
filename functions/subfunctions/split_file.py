a40757d83b04b33a19ecc4c24949601165b8111a
Adrian Deleon <adr.m.del@gmail.com>
2018-04-05 17:43:27 -0400

    revert filepath

--- functions/subfunctions/split_file.py
+++ functions/subfunctions/split_file.py
@@ -4,6 +4,6 @@ cur_path = os.path.dirname(__file__)
 
 
 def split_file(filename):
-    file = open(cur_path + '\..\..\\textdbs\\' + filename + '.txt', 'r')
+    file = open(cur_path + '/../../textdbs/' + filename + '.txt', 'r')
     this_list = file.readline().split('|||')
     return this_list
