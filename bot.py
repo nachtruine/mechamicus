642efe5a492895a067aec7791ddf2daf79ee903d
Adrian Deleon <adr.m.del@gmail.com>
2018-05-07 23:40:58 -0700

    new memory function

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import discord
 import sys
 
 from datetime import datetime
-from functions import db, deck, dice, gcs, quote
+from functions import db, deck, dice, gcs, quote, remember
 from functions.dice import total_dice_regex
 from functions.subfunctions.split_file import split_file
 
@@ -98,6 +98,11 @@ def parse_command(msg):
     elif command == 'quote':
         if conn is not None:
             return quote.parse_quote_request(msg, conn)
+        else:
+            return 'I cannot access the database right now.'
+    elif command == 'remember':
+        if conn is not None:
+            return remember.parse_remember_request(msg, conn)
         else:
             return 'I cannot access the database right now.'
 
---
+++ functions/remember.py
@@ -0,0 +1,30 @@
+def parse_remember_request(msg, conn):
+    command = str(msg.clean_content)[len('/remember '):]
+    serv_id = str(msg.server.id)
+    cursor = conn.cursor()
+    if command.startswith('add'):
+        trash, memory_id, content = command.split(' ', 2)
+        cursor.execute('''
+        INSERT INTO Memory(id, serv_id, text)
+        VALUES(%s, %s, %s, %s)
+        ''', (memory_id, serv_id, content))
+        conn.commit()
+        return 'I will remember that!'
+    elif command.startswith('update'):
+        trash, memory_id, content = command.split(' ', 2)
+        cursor.execute('''
+                UPDATE Memory
+                SET text = %s
+                WHERE id = %s
+                AND serv_id = %s
+                ''', (content, memory_id, serv_id))
+        conn.commit()
+        return 'I\'ll update my memory!'
+    else:
+        cursor.execute('''
+        SELECT text FROM Memory
+        WHERE serv_id = %s
+        AND LOWER(id) = LOWER(%s)
+        ''', (serv_id, command))
+        for i in cursor:  # will only ever have one result
+            return i
