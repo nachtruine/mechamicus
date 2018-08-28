f7739baff4ba45185c767b217abe5cfd547c4856
Adrian Deleon <adr.m.del@gmail.com>
2018-08-27 22:19:21 -0700

    significantly simplified dice rolling

--- functions/dice.py
+++ functions/dice.py
@@ -1,8 +1,6 @@
 import random
 import re
-__single_dice_regex = r'^([+-]?)(\d+)d(\d+)|([+-])(\d+)$'
-total_dice_regex = r'^([+-]?(?:\d+|\d*d(?:\d+))(?:[+-](?:\d+|\d*d(?:\d+)))*)( .+)?$'
-__bad_regex = r'^([+-])(\d+)$'
+dice_regex = r'(?:([+-]?)(\d+)d(\d+))|(?:([+-]\d+))| (.+$)'
 listResults = []
 
 
@@ -10,66 +8,18 @@ def __roll_dice(mod, num, sides):
     result = 0
     num = int(num)
 
-    if sides == -1:
-        if mod.casefold() == '+':
-            result = num
-        elif mod.casefold() == '-':
-            result = num * -1
-        return result
-
-    else:
-        sides = int(sides)
-        if mod.casefold() == '' or mod.casefold() == '+':
-            for i in range(num):
-                roll = random.randint(1, sides)
-                listResults.append(roll)
-                result += roll
-        elif mod.casefold() == '-':
-            for i in range(num):
-                roll = random.randint(1, sides)
-                listResults.append(roll)
-                result -= roll
-        return result
-
-
-def __parse_single_request(cmd):
-    single_search = re.match(__single_dice_regex, cmd)
-    mod = single_search.group(1)
-    num = single_search.group(2)
-    sides = single_search.group(3)
-    flatmod = single_search.group(4)
-    flatnum = single_search.group(5)
-    if flatmod is None and flatnum is None:
-        result = __roll_dice(mod, num, sides)
-        return result
-    elif mod is None and num is None and sides is None:
-        result = __roll_dice(flatmod, flatnum, -1)
-        return result
-
-
-def __remove_used_request(cmd):
-    length = 1
-
-    single_search = re.match(__single_dice_regex, cmd)
-
-    mod = single_search.group(1)
-    num = single_search.group(2)
-    sides = single_search.group(3)
-    flatmod = single_search.group(4)
-    flatnum = single_search.group(5)
-
-    if mod is not None:
-        length += len(mod)
-    if num is not None:
-        length += len(num)
-    if sides is not None:
-        length += len(sides)
-    if flatmod is not None:
-        length += len(flatmod)
-    if flatnum is not None:
-        length += len(flatnum)
-    cmd = cmd[length:]
-    return cmd
+    sides = int(sides)
+    if mod.casefold() == '' or mod.casefold() == '+':
+        for i in range(num):
+            roll = random.randint(1, sides)
+            listResults.append(roll)
+            result += roll
+    elif mod.casefold() == '-':
+        for i in range(num):
+            roll = random.randint(1, sides)
+            listResults.append(roll)
+            result -= roll
+    return result
 
 
 def parse_dice_request(msg):
@@ -77,29 +27,19 @@ def parse_dice_request(msg):
     comment = ''
     if str(msg.content).startswith('/roll '):
         content = content[len('/roll '):]
-
-    try:
-        command, comment = content.split(" ", 1)
-    except ValueError:
-        command = content
-
-    if re.search(__bad_regex, command) is not None:
-        return None
-
+    rolls_to_process = re.findall(dice_regex, content)
     roll_result = 0
-
-    while command is not "":
-        try:
-            add_result = __parse_single_request(command)
-            roll_result += add_result
-        except AttributeError:
-            return None
-        command = __remove_used_request(command)
-
+    for x in rolls_to_process:
+        if x[3] == '' and x[4] == '':
+            roll_result += __roll_dice(x[0], x[1], x[2])
+        elif x[3] != '' and x[4] == '':
+            roll_result += int(x[3])
+        else:
+            comment = x[4]
     if len(comment) > 0:
         end_result = '\"' + comment + '\": ' + str(listResults) + ' -> **' + str(roll_result) + '**'
     else:
         end_result = str(listResults) + ' -> **' + str(roll_result) + '**'
-
+    print(end_result)
     listResults.clear()
     return end_result
